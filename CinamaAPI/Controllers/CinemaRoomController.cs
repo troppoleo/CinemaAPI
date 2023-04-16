b32c48009ce276b5cbd140ca528b3e427350baa2
leo <troppoleo@gmail.com>
2023-04-16 16:10:59 +0200

    Aggiunto CinemaRoom

---
+++ CinamaAPI/Controllers/CinemaRoomController.cs
@@ -0,0 +1,25 @@
+﻿using CinemaBL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CinemaRoomController : ControllerBase
+    {
+        private readonly ICinemaRoomService _cr;
+
+        public CinemaRoomController(CinemaBL.ICinemaRoomService cr)
+        {
+            _cr = cr;
+        }
+
+        [HttpGet(Name = "GetCinemaRooms")]
+        public IEnumerable<CinemaDAL.Models.CinemaRoom> GetCinemaRooms()
+        { 
+            return _cr.GetCinemaRooms();
+        }
+
+    }
+}
---
+++ CinemaBL/CinemaRoomService.cs
@@ -0,0 +1,28 @@
+﻿using CinemaDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaBL
+{
+    public interface ICinemaRoomService
+    {
+        List<CinemaRoom> GetCinemaRooms();
+    }
+
+    public class CinemaRoomService : ICinemaRoomService
+    {
+        private readonly CinemaContext _ctx;
+
+        public CinemaRoomService(CinemaDAL.Models.CinemaContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public List<CinemaDAL.Models.CinemaRoom> GetCinemaRooms()
+        {
+            return _ctx.CinemaRooms.ToList();
+        }
+    }
+}
